5719b37b34865b828bb526779721e0dffc229a4f
Loochs <54494160+loochs@users.noreply.github.com>
2020-06-25 22:40:40 +0900

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,35 @@
+let 右向き = 0
+let コースを外れた = 0
+let 左向き = 0
+input.onButtonPressed(Button.A, function () {
+	
+})
+function ラインから外れた () {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+        右向き = 1
+        コースを外れた = 0
+    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+        左向き = 1
+        コースを外れた = 0
+    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+        コースを外れた = 1
+    } else {
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+        右向き = 0
+        左向き = 0
+        コースを外れた = 1
+    }
+}
+basic.forever(function () {
+    ラインから外れた()
+    if (true) {
+    	
+    }
+})
